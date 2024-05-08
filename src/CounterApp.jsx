96d45e1b2299f01ada2b1f2cdd6cdbf21d21e370
martinrdrz <martinrdrz@hotmail.com>
2024-05-08 17:49:56 -0300

    modificaciones menores

---
+++ src/CounterApp.jsx
@@ -0,0 +1,24 @@
+import { PropTypes } from "prop-types";
+
+export const CounterApp = ({ value }) => {
+  const handleAdd = (event) => {
+    console.log(event);
+    value = value + 1;
+  };
+
+  return (
+    <>
+      <h1>CounterApp</h1>
+      <h2>{value}</h2>
+      <button onClick={handleAdd}>+1</button>
+    </>
+  );
+};
+
+CounterApp.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+CounterApp.dafaultProps = {
+  value: 0,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from "./FirstApp";
+import { CounterApp } from "./CounterApp";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp title="Hola Soy Goku" subTitle={123} />
+    <CounterApp value={10} />
   </React.StrictMode>
 );
