4b8dd5ab77b6ad5632868348b13772fbd4c16a91
martinrdrz <martinrdrz@hotmail.com>
2024-05-09 18:52:24 -0300

    se agrego jest y sus configuracion con babel para ejecutar codigo moderno de JS

---
+++ tests/demo.test.js
@@ -0,0 +1,8 @@
+describe("Pruebas en <DemoComponent>", () => {
+  test("Esta prueba no debe de fallar", () => {
+    const message1 = "Hola Mundo";
+    const message2 = message1.trim();
+
+    expect(message1).toBe(message2);
+  });
+});
